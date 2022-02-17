d1a8a71432c9b927f8d222df59826e3e799bb5f2
Ekas_ <karuniawankaka80@mail.ugm.ac.id>
2022-02-17 21:59:27 +0700

    project init

---
+++ app/src/main/java/apps/jobcourse/broadcastproject/CustomReceiver.java
@@ -0,0 +1,16 @@
+package apps.jobcourse.broadcastproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "this function is triggered", Toast.LENGTH_SHORT).show();
+    }
+}
