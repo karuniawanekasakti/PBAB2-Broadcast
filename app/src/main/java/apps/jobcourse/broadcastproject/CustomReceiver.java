2bdc7c1ef7227ee0347f74c6ba74f723dddb2ba7
Ekas_ <karuniawankaka80@mail.ugm.ac.id>
2022-02-17 23:10:19 +0700

    broadcast project

--- app/src/main/java/apps/jobcourse/broadcastproject/CustomReceiver.java
+++ app/src/main/java/apps/jobcourse/broadcastproject/CustomReceiver.java
@@ -11,6 +11,28 @@ public class CustomReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        Toast.makeText(context, "this function is triggered", Toast.LENGTH_SHORT).show();
+
+        String msg= "This is default message";
+        switch (intent.getAction()){
+            case Intent.ACTION_POWER_CONNECTED:
+                msg = "The power is connected!";
+                break;
+
+            case Intent.ACTION_POWER_DISCONNECTED:
+                msg = "The power is disconnected!";
+                break;
+
+            case "ACTION_CUSTOM_BROADCAST":
+                msg = "Custom broadcast received";
+                break;
+
+            case "ACTION_DATA_BROADCAST":
+                msg = intent.getStringExtra("Data");
+                break;
+
+            default:
+                break;
+        }
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/apps/jobcourse/broadcastproject/MainActivity.java
+++ app/src/main/java/apps/jobcourse/broadcastproject/MainActivity.java
@@ -1,14 +1,20 @@
 package apps.jobcourse.broadcastproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private CustomReceiver mCustomReceiver = new CustomReceiver();
+
+    private Button sendBtn;
+    private Button dataBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +25,38 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
 
         registerReceiver(mCustomReceiver, intentFilter);
+
+        sendBtn = findViewById(R.id.send_button);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("ACTION_CUSTOM_BROADCAST");
+                LocalBroadcastManager.getInstance(view.getContext()).sendBroadcast(intent);
+            }
+        });
+
+        dataBtn = findViewById(R.id.data_button);
+        dataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("ACTION_DATA_BROADCAST");
+                intent.putExtra("Data", "This is the data");
+                LocalBroadcastManager.getInstance(view.getContext()).sendBroadcast(intent);
+            }
+        });
+
+        IntentFilter intentFilter2 = new IntentFilter("ACTION_DATA_BROADCAST");
+        LocalBroadcastManager.getInstance(this).registerReceiver(mCustomReceiver,intentFilter2);
+
+        IntentFilter intentFilter1 = new IntentFilter("ACTION_CUSTOM_BROADCAST");
+        LocalBroadcastManager.getInstance(this).registerReceiver(mCustomReceiver, intentFilter1);
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(mCustomReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mCustomReceiver);
     }
 }
